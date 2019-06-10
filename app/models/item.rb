359920577ae279bc68dd4a62dfed2888d82cdd21
Simon <semen.metelev@gmail.com>
2019-06-10 03:34:43 -0400

    Alter maxlength for description of Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   before_save :create_img_digest
   validates :price, length: { maximum: 6 }, presence: true
   validates :name, presence: true, uniqueness: true
-  validates :description, presence: true
+  validates :description, presence: true, length: { maximum: 500 }
   def create_img_digest
     self.img_digest = Digest::MD5.hexdigest(self.name)
   end
