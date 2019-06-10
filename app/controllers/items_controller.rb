bcf0b4f67d8c21f476d884759487b2a1ad0deb1c
Simon <semen.metelev@gmail.com>
2019-06-10 07:38:43 -0400

    Implement creating items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,9 @@
 class ItemsController < ApplicationController
   def create
-
+    name = params['name']
+    description = params['description']
+    price = params['price']
+    Item.create(name: name, description: description, price: price)
   end
   def generate_digest
     name = params['name']
@@ -8,7 +11,7 @@ class ItemsController < ApplicationController
       img_digest = Digest::MD5.hexdigest(name)
       render json: { success: 'true', digest: img_digest}
     else
-      render json: {success: 'false'}
+      render json: { success: 'false' }
     end
   end
 end
